94d7e7fbb8366d230f7dd0a6fb229ec02019927d
Etienne Cherrier <Etienne.Cherrier@PDGM.com>
2019-11-24 17:39:30 +0100

    include rotation, polygons, and transparency

--- fpdf/fpdf.php
+++ fpdf/fpdf.php
@@ -1922,10 +1922,21 @@ protected function _putresourcedict()
 	$this->_put('/XObject <<');
 	$this->_putxobjectdict();
 	$this->_put('>>');
+
+	// from alpha channel
+	$this->_put('/ExtGState <<');
+	foreach($this->extgstates as $k=>$extgstate)
+		$this->_put('/GS'.$k.' '.$extgstate['n'].' 0 R');
+	$this->_put('>>');
 }
 
 protected function _putresources()
 {
+	// from alpha channel
+	$this->_putextgstates();
+
+	////////////////////////
+	
 	$this->_putfonts();
 	$this->_putimages();
 	// Resource dictionary
@@ -1979,6 +1990,10 @@ protected function _puttrailer()
 
 protected function _enddoc()
 {
+	
+	if(!empty($this->extgstates) && $this->PDFVersion<'1.4')
+		$this->PDFVersion='1.4';
+
 	$this->_putheader();
 	$this->_putpages();
 	$this->_putresources();
@@ -2011,5 +2026,108 @@ protected function _enddoc()
 	$this->_put('%%EOF');
 	$this->state = 3;
 }
+
+// extends Polygons
+
+function Polygon($points, $style='D')
+{
+    //Draw a polygon
+    if($style=='F')
+        $op = 'f';
+    elseif($style=='FD' || $style=='DF')
+        $op = 'b';
+    else
+        $op = 's';
+
+    $h = $this->h;
+    $k = $this->k;
+
+    $points_string = '';
+    for($i=0; $i<count($points); $i+=2){
+        $points_string .= sprintf('%.2F %.2F', $points[$i]*$k, ($h-$points[$i+1])*$k);
+        if($i==0)
+            $points_string .= ' m ';
+        else
+            $points_string .= ' l ';
+    }
+    $this->_out($points_string . $op);
+}
+
+// extends alpha channel
+
+
+protected $extgstates = array();
+
+// alpha: real value from 0 (transparent) to 1 (opaque)
+// bm:    blend mode, one of the following:
+//          Normal, Multiply, Screen, Overlay, Darken, Lighten, ColorDodge, ColorBurn,
+//          HardLight, SoftLight, Difference, Exclusion, Hue, Saturation, Color, Luminosity
+function SetAlpha($alpha, $bm='Normal')
+{
+	// set alpha for stroking (CA) and non-stroking (ca) operations
+	$gs = $this->AddExtGState(array('ca'=>$alpha, 'CA'=>$alpha, 'BM'=>'/'.$bm));
+	$this->SetExtGState($gs);
+}
+
+function AddExtGState($parms)
+{
+	$n = count($this->extgstates)+1;
+	$this->extgstates[$n]['parms'] = $parms;
+	return $n;
+}
+
+function SetExtGState($gs)
+{
+	$this->_out(sprintf('/GS%d gs', $gs));
+}
+
+function _putextgstates()
+{
+	for ($i = 1; $i <= count($this->extgstates); $i++)
+	{
+		$this->_newobj();
+		$this->extgstates[$i]['n'] = $this->n;
+		$this->_put('<</Type /ExtGState');
+		$parms = $this->extgstates[$i]['parms'];
+		$this->_put(sprintf('/ca %.3F', $parms['ca']));
+		$this->_put(sprintf('/CA %.3F', $parms['CA']));
+		$this->_put('/BM '.$parms['BM']);
+		$this->_put('>>');
+		$this->_put('endobj');
+	}
+}
+
+// extends rotate
+
+var $angle=0;
+
+function Rotate($angle,$x=-1,$y=-1)
+{
+    if($x==-1)
+        $x=$this->x;
+    if($y==-1)
+        $y=$this->y;
+    if($this->angle!=0)
+        $this->_out('Q');
+    $this->angle=$angle;
+    if($angle!=0)
+    {
+        $angle*=M_PI/180;
+        $c=cos($angle);
+        $s=sin($angle);
+        $cx=$x*$this->k;
+        $cy=($this->h-$y)*$this->k;
+        $this->_out(sprintf('q %.5F %.5F %.5F %.5F %.2F %.2F cm 1 0 0 1 %.2F %.2F cm',$c,$s,-$s,$c,$cx,$cy,-$cx,-$cy));
+    }
+}
+
+function RotatedText($x,$y,$txt,$angle)
+{
+    //Text rotated around its origin
+    $this->Rotate($angle,$x,$y);
+    $this->Text($x,$y,$txt);
+    $this->Rotate(0);
+}
+
 }
 ?>
